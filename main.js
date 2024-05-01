6b87f60c651d294f22425afc6f69802280f04333
ashok <ashokkumardc21@gmail.com>
2024-05-01 08:31:00 +0530

    updated

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     loop:true,
-    // autoplay: {
-    //   delay: 3500,
-    //   disableOnInteraction: false,
-    // },
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
